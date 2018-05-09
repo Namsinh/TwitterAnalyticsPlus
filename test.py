412246b1f5a323de125f5ee2265b158d2ca6a654
Ben Wasserman <wasse114@mail.chapman.edu>
2018-05-08 19:59:46 -0700

    Adds mutliple search functionality

--- test.py
+++ test.py
@@ -24,76 +24,96 @@ auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-# I/O Placeholder
-print (
-    "This tool will help you analyze a public account on twitter based " +
-    " on the accounts they follow.")
-name = raw_input('Username on Twitter(public accounts only): ')
+while(True):
+    start = raw_input("Press 1 to look at the tweets of a specific user, " +
+                      " and 2 to look at all the tweets of a user's followers\n")
+    if (start == '1'):
 
-user = api.get_user(name)
+        # I/O Placeholder
+        print (
+            "This tool will help you analyze a public account on twitter based " +
+            " on the accounts they follow.")
+        name = raw_input('Username on Twitter(public accounts only): ')
 
-my_screen_name = user.screen_name
-print(my_screen_name)
+        user = api.get_user(name)
 
-print ()
-print ("****** Testing the twitter anaylytics platform ********")
-print ()
+        my_screen_name = user.screen_name
+        print(my_screen_name)
 
-print ("First, let's see the categories you tweet about most")
-print ()
+        print ()
+        print ("****** Testing the twitter anaylytics platform ********")
+        print ()
 
-my_tweets = ""
-personal_tweets = api.user_timeline(screen_name=my_screen_name,
-                                    count=30, tweet_mode="extended")
+        print ("First, let's see the categories you tweet about most")
+        print ()
 
-for x in personal_tweets:
-    # print (x.full_text)
-    my_tweets += x.full_text
-try:
-    simple_content.simple_classify(my_tweets)
-except:
-    print("Language not supported by Google Cloud")
+        my_tweets = ""
+        personal_tweets = api.user_timeline(screen_name=my_screen_name,
+                                            count=30, tweet_mode="extended")
 
-simple_content.print_categories()
-
-# Initialize a list here
-
-# Define a function to parse all the information from the list
-
-# A dictionary for each user's screen name and their tweets
-user_tweets = {}
-
-count = 0
-
-print("First, we will analyze the tweets of the following people you follow")
-
-for friend in user.friends(count=50):
-    # print ("Follower: " + friend.screen_name + "\n")
-    # print (friend.screen_name)
-    if (count == 0):
-        print ("Reading first 5 users right now")
-    elif (count % 5 == 0):
-        print ("Reading users " + str(count) + " - " + str(count+5) +
-               " right now")
-    count += 1
-    search = friend.screen_name
-    print (search)
-    try:
-        all_tweets = ""
-        tweets = api.user_timeline(
-            screen_name=search, count=10, tweet_mode="extended")
-        for tweet in tweets:
-            all_tweets += tweet.full_text
+        for x in personal_tweets:
+            # print (x.full_text)
+            my_tweets += x.full_text
         try:
-            simple_content.simple_classify(all_tweets)
+            simple_content.simple_classify(my_tweets)
         except:
-            print ("A friend was skipped due to blocked permissions")
+            print("Language not supported by Google Cloud")
 
-        # Create dictionary with everything
-        user_tweets[search] = all_tweets
+        simple_content.print_categories()
 
-    except tweepy.TweepError:
-        print ("This user has protected tweets. Failed to run")
+    elif (start == '2'):
 
+        name = raw_input('Username on Twitter(public accounts only): ')
 
-simple_content.print_categories()
+        user = api.get_user(name)
+
+        # Initialize a list here
+
+        # Define a function to parse all the information from the list
+
+        # A dictionary for each user's screen name and their tweets
+        user_tweets = {}
+
+        count = 0
+
+        print("First, we will analyze the tweets of the following people you follow")
+
+        for friend in user.friends(count=50):
+            # print ("Follower: " + friend.screen_name + "\n")
+            # print (friend.screen_name)
+            if (count == 0):
+                print ("Reading first 5 users right now")
+            elif (count % 5 == 0):
+                print ("Reading users " + str(count) + " - " + str(count+5) +
+                       " right now")
+            count += 1
+            search = friend.screen_name
+            print (search)
+            try:
+                all_tweets = ""
+                tweets = api.user_timeline(
+                    screen_name=search, count=10, tweet_mode="extended")
+                for tweet in tweets:
+                    all_tweets += tweet.full_text
+                try:
+                    simple_content.simple_classify(all_tweets)
+                except:
+                    print ("A friend was skipped due to blocked permissions")
+
+                # Create dictionary with everything
+                user_tweets[search] = all_tweets
+
+            except tweepy.TweepError:
+                print ("This user has protected tweets. Failed to run")
+
+        simple_content.print_categories()
+
+    else:
+        print("Try again.")
+
+    continuation = raw_input("If you would like to continue, press y. " +
+                             " Press any other botton to quit")
+
+    if (continuation != 'y'):
+        print ("Thanks for using our analytics platform")
+        break
